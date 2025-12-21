0a59054ad35d7d0536fcb07fa6be5981c2c3570a
Chris <chriskh369@gmail.com>
2025-12-21 19:20:40 +0200

    Add refresh button and disable caching in app

--- android/app/src/main/java/com/chris/studyhub/MainActivity.java
+++ android/app/src/main/java/com/chris/studyhub/MainActivity.java
@@ -1,5 +1,17 @@
 package com.chris.studyhub;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Disable cache to always get fresh content
+        WebView webView = getBridge().getWebView();
+        webView.getSettings().setCacheMode(android.webkit.WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setAppCacheEnabled(false);
+    }
+}
